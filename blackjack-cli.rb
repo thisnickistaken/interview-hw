0b610a9067d4f3247ef93c5daafea53ba2a901ff
Steven Johnson <steven.gregory.johnson@gmail.com>
2014-03-08 22:39:31 -0600

    moved to a more interactive one-shot version of blackjack

--- blackjack-cli.rb
+++ blackjack-cli.rb
@@ -6,26 +6,50 @@ include BlackJack
 
 game = Game.new()
 
-puts game.add_player("Steven", 1000.00);
-puts game.shuffle_deck()
+while !print("Enter player name, leave blank when done: ") && ((player = gets.chomp()) != "") do
+	print "Enter balance for #{player}: "
+	game.add_player(player, gets.chomp().to_f());
+end
 
-puts game.place_bet("Steven", 500.00);
+if ret = game.shuffle_deck() != 0
+	puts "Error: Failed to shuffle deck: #{error_to_str(ret)}"
+	exit 1
+end
 
-puts game.deal()
-
-while game.playing("Steven") do
-	print "What would you like to do? "
-	case ret = game.play_hand("Steven", str_to_action(gets.chomp()))
+game.each_player do |name|
+	printf "#{name}, enter your wager: "
+	case ret = game.place_bet(name, gets.chomp().to_f())
 		when 0
-		when BJE_ACTION || BJE_BET || BJE_FIRST || BJE_SPLIT
+		when BJE_BET || BJE_NEG_BET
 			puts error_to_str(ret)
 		else
 			puts "Fatal: #{error_to_str(ret)}"
+			exit 1
 	end
 end
 
-while game.dealer_playing() do
+game.deal()
+
+game.each_player do |name|
+	game.playing_loop(name) do
+		game.print_player(name)
+		print "#{name}, what would you like to do? "
+		case ret = game.play_hand(name, str_to_action(gets.chomp()))
+			when 0
+			when BJE_ACTION || BJE_BET || BJE_FIRST || BJE_SPLIT
+				puts error_to_str(ret)
+			else
+				puts "Fatal: #{error_to_str(ret)}"
+				exit 1
+		end
+	end
+end
+
+game.dealer_playing_loop do
+	game.print_dealer()
 	game.play_dealer()
 end
 
-puts game.resolve()
+game.resolve()
+
+game.print()
