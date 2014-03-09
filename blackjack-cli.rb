924be27d32841c7d3b8006325345daf1c6b16a38
Steven Johnson <steven.gregory.johnson@gmail.com>
2014-03-08 23:29:24 -0600

    added error checking; loop around place_bet on error; fixed case

--- blackjack-cli.rb
+++ blackjack-cli.rb
@@ -11,24 +11,29 @@ while !print("Enter player name, leave blank when done: ") && ((player = gets.ch
 	game.add_player(player, gets.chomp().to_f());
 end
 
-if ret = game.shuffle_deck() != 0
+unless ret = game.shuffle_deck() == 0
 	puts "Error: Failed to shuffle deck: #{error_to_str(ret)}"
 	exit 1
 end
 
 game.each_player do |name|
-	printf "#{name}, enter your wager: "
-	case ret = game.place_bet(name, gets.chomp().to_f())
-		when 0
-		when BJE_BET || BJE_NEG_BET
-			puts error_to_str(ret)
-		else
-			puts "Fatal: #{error_to_str(ret)}"
-			exit 1
-	end
+	begin
+		printf "#{name}, enter your wager: "
+		case ret = game.place_bet(name, gets.chomp().to_f())
+			when 0
+			when BJE_BET, BJE_NEG_BET
+				puts error_to_str(ret)
+			else
+				puts "Fatal: #{error_to_str(ret)}"
+				exit 1
+		end
+	end while ret != 0
 end
 
-game.deal()
+unless ret = game.deal() == 0
+	puts "Error: #{error_to_str(ret)}"
+	exit 1
+end
 
 game.each_player do |name|
 	game.playing_loop(name) do
@@ -36,7 +41,7 @@ game.each_player do |name|
 		print "#{name}, what would you like to do? "
 		case ret = game.play_hand(name, str_to_action(gets.chomp()))
 			when 0
-			when BJE_ACTION || BJE_BET || BJE_FIRST || BJE_SPLIT
+			when BJE_ACTION, BJE_BET, BJE_FIRST, BJE_SPLIT
 				puts error_to_str(ret)
 			else
 				puts "Fatal: #{error_to_str(ret)}"
@@ -47,9 +52,15 @@ end
 
 game.dealer_playing_loop do
 	game.print_dealer()
-	game.play_dealer()
+	unless ret = game.play_dealer() == 0
+		puts "Error: #{error_to_str(ret)}"
+		exit 1
+	end
 end
 
-game.resolve()
+unless ret = game.resolve() == 0
+	puts "Error: #{error_to_str(ret)}"
+	exit 1
+end
 
 game.print()
