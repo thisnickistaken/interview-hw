21f9a87a86875579da1e03a69a999200d5db9bbe
Steven Johnson <steven.gregory.johnson@gmail.com>
2014-03-09 17:12:05 -0500

    added check for blackjack.h

--- BlackJack/extconf.rb
+++ BlackJack/extconf.rb
@@ -5,6 +5,7 @@ require "mkmf"
 extension_name = "blackjack"
 dir_config(extension_name)
 
+have_header("blackjack.h") or abort "blackjack.h is missing!"
 have_library("blackjack") or abort "libblackjack is missing!"
 
 create_makefile(extension_name)
